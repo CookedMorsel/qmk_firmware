fd46ac0f6bb4ce9f8f1df355ab4c8f10f9edbf21
Rotem Yaari <vmalloc@gmail.com>
2019-09-08 12:57:37 +0300

    Tap dance Del/F9

--- keyboards/massdrop/alt/keymaps/vmalloc/keymap.c
+++ keyboards/massdrop/alt/keymaps/vmalloc/keymap.c
@@ -12,12 +12,16 @@ enum alt_keycodes {
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO  // Toggle 6KRO / NKRO mode
 
+enum { TD_DEL_F9 = 0 };
+
+qk_tap_dance_action_t tap_dance_actions[] = {[TD_DEL_F9] = ACTION_TAP_DANCE_DOUBLE(KC_DEL, KC_F9)};
+
 keymap_config_t keymap_config;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TD(TD_DEL_F9), \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
